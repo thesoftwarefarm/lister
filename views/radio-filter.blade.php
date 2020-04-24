4c502f05f01f9331b62517c19aed29aa3efa68e7
Ionut Antohi <ionut@thesoftwarefarm.co.uk>
2020-04-24 08:53:59 +0300

    fix selected value for radio and select when items contains a key = 0

--- views/radio-filter.blade.php
+++ views/radio-filter.blade.php
@@ -6,7 +6,7 @@
     <div>
         @foreach($items as $key => $item)
             <div class="custom-control custom-control-inline custom-radio">
-                <input type="radio" class="custom-control-input filter_{{ \Illuminate\Support\Str::kebab($input_name) }}" name="{{ $input_name }}" id="radio_{{ \Illuminate\Support\Str::kebab($input_name) }}_{{ $key }}" value="{{ $key }}" @if($key == $search_keyword) checked @endif>
+                <input type="radio" class="custom-control-input filter_{{ \Illuminate\Support\Str::kebab($input_name) }}" name="{{ $input_name }}" id="radio_{{ \Illuminate\Support\Str::kebab($input_name) }}_{{ $key }}" value="{{ $key }}" @if(!is_null($search_keyword) && $key == $search_keyword) checked @endif>
                 <label class="custom-control-label" for="radio_{{ \Illuminate\Support\Str::kebab($input_name) }}_{{ $key }}">{{ $item }}</label>
             </div>
         @endforeach
